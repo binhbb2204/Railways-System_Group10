ec2511400afab817d05064d2fbfec357e35d538d
lilmonkeygoturback <quantruong0307@gmail.com>
2024-03-13 21:45:12 +0700

    Test Done

--- src/main/Main.java
+++ src/main/Main.java
@@ -32,8 +32,7 @@ public class Main {
             java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //draft
-        System.out.println("Hello, World!");
+
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
