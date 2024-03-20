ee705e06d034e0b45cc73e47ae3411f205f0b2ff
binhbb2204 <binhbb2204@gmail.com>
2024-03-20 18:21:25 +0700

    making model_menu

--- src/model/Model_Menu.java
+++ src/model/Model_Menu.java
@@ -3,12 +3,44 @@ package model;
 
 
 public class Model_Menu {
-    String icon;
-    String name;
-    MenuType type;
+
+    private String icon;
+    private String name;
+    private MenuType type;
     
+    public Model_Menu(String icon, String name, MenuType type) {
+        this.icon = icon;
+        this.name = name;
+        this.type = type;
+    }
     
     public static enum MenuType {
         TITLE, MENU, EMPTY
     }
+    
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public MenuType getType() {
+        return type;
+    }
+    
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(MenuType type) {
+        this.type = type;
+    }
+    
+    
 }
