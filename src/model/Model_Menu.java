35146d43da2187ac0a1cc93d48eb5b134097df8c
binhbb2204 <binhbb2204@gmail.com>
2024-03-20 18:16:32 +0700

    making model menu

--- src/model/Model_Menu.java
+++ src/model/Model_Menu.java
@@ -5,10 +5,10 @@ package model;
 public class Model_Menu {
     String icon;
     String name;
-    
+    MenuType type;
     
     
     public static enum MenuType {
-        
+        TITLE, MENU, EMPTY
     }
 }
