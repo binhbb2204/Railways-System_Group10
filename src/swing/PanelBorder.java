ff2bbf126ca29299b9389ff5d29a5d9d89553a78
binhbb2204 <binhbb2204@gmail.com>
2024-03-20 16:07:01 +0700

    making panel border

--- src/swing/PanelBorder.java
+++ src/swing/PanelBorder.java
@@ -1,10 +1,12 @@
 
 package swing;
+import java.awt.*;
 
 public class PanelBorder extends javax.swing.JPanel {
 
     public PanelBorder() {
         initComponents();
+        setOpaque(false);
     }
 
     @SuppressWarnings("unchecked")
@@ -24,6 +26,13 @@ public class PanelBorder extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    // End of variables declaration//GEN-END:variables
+    @Override
+    protected void paintComponent(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        super.paintComponent(g);
+
+    }
 }
