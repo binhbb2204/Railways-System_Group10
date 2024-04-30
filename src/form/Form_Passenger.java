801877331cae8cdb3ba7e162e03b0ffdb8b04902
Vũ Thành Nhân <147362828+VuThanhNhan2003@users.noreply.github.com>
2024-04-30 23:02:09 +0700

    making passenger form

---
+++ src/form/Form_Passenger.java
@@ -0,0 +1,232 @@
+
+package form;
+
+import java.awt.Color;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+
+import swing.AddingActionEvent;
+import swing.ScrollBar;
+import swing.TableActionCellEditor;
+import swing.TableActionCellRender;
+import swing.TableActionEvent;
+import model.Model_Card;
+import model.PassengerStatus;
+import model.StatusType;
+
+public class Form_Passenger extends javax.swing.JPanel {
+    private boolean editable = false;
+    private int editableRow = -1;
+    public Form_Passenger() {
+        initComponents();
+        AddingActionEvent event1 = new AddingActionEvent() {
+            @Override
+            public void onAdding(int row) {
+                DefaultTableModel model = (DefaultTableModel) passengerTable1.getModel();
+                model.addRow(new Object[]{"", "", "", "", "", "", PassengerStatus.TICKETED});
+                model.fireTableDataChanged();
+                
+            }
+            
+        };
+        cmdAdding.initEvent(event1, 0);
+
+        TableActionEvent event = new TableActionEvent() {
+            @Override
+            public void onEdit(int row) {
+                editableRow = row;
+                editable = true;
+                ((DefaultTableModel)passengerTable1.getModel()).fireTableDataChanged();
+
+                passengerTable1.repaint();
+                passengerTable1.revalidate();
+                
+                
+            }
+            @Override
+            public void onDelete(int row) {
+                if(passengerTable1.isEditing()){
+                    passengerTable1.getCellEditor().stopCellEditing();
+                }
+                DefaultTableModel model = (DefaultTableModel) passengerTable1.getModel();
+                model.removeRow(row);
+            }
+            @Override
+            public void onView(int row) {
+                editableRow = row;
+                editable = false;
+                ((DefaultTableModel)passengerTable1.getModel()).fireTableDataChanged();
+                
+                passengerTable1.repaint();
+                passengerTable1.revalidate();
+                
+            }
+            
+            
+        };
+        passengerTable1.getColumnModel().getColumn(6).setCellRenderer(new TableActionCellRender());
+        passengerTable1.getColumnModel().getColumn(6).setCellEditor(new TableActionCellEditor(event));
+
+
+
+
+
+
+
+        card1.setData(new Model_Card(new ImageIcon(getClass().getResource("/icons/profit.png")), "Total profit", "₫ 9,112,001,000", "increased by 5%"));
+        card2.setData(new Model_Card(new ImageIcon(getClass().getResource("/icons/transport.png")), "Ticket Price", "₫ 80,000", "Price can be changed by the occasion"));
+        card3.setData(new Model_Card(new ImageIcon(getClass().getResource("/icons/train-station.png")), "Total Passenger Count", "131,227", "increased by 5%"));
+        
+        //add row table
+        spTable.setVerticalScrollBar(new ScrollBar());
+        spTable.getVerticalScrollBar().setBackground(Color.WHITE);
+        spTable.getViewport().setBackground(Color.WHITE);
+        JPanel p = new JPanel();
+        p.setBackground(Color.WHITE);
+        spTable.setCorner(JScrollPane.UPPER_RIGHT_CORNER, p);
+        //table.getColumModel is used for the status column because it's a JComboBox
+        passengerTable1.getColumnModel().getColumn(5).setCellEditor(new DefaultCellEditor(new JComboBox<>(PassengerStatus.values())));
+        passengerTable1.addRow(new Object[]{"01","Tran", "Thanh An", "0983127301", "thanhan@gmail.com",PassengerStatus.TICKETED});
+        passengerTable1.addRow(new Object[]{"02","Nguyen", "Le Binh", "0957483948", "lebinh@gmail.com",PassengerStatus.TICKETED});
+        passengerTable1.addRow(new Object[]{"03","Pham", "Van Cuong", "0956473849", "vancuong@gmail.com",PassengerStatus.CANCELLED});
+        passengerTable1.addRow(new Object[]{"04","Le", "Trang Dao", "0984930293", "trangdao@gmail.com",PassengerStatus.TICKETED});
+        passengerTable1.addRow(new Object[]{"05","Ly", "Minh Thong", "0949303938", "minhthong@gmail.com",PassengerStatus.ARRIVED});
+        passengerTable1.addRow(new Object[]{"06","Nguyen", "Tien Dat", "0955584939", "tiendat@gmail.com",PassengerStatus.TICKETED});
+        passengerTable1.addRow(new Object[]{"07","Mike", "Tyson", "0984877449", "mike@gmail.com",PassengerStatus.ARRIVED});
+        passengerTable1.addRow(new Object[]{"08","Mc", "John", "0911122345", "john@gmail.com",PassengerStatus.CANCELLED});
+        passengerTable1.addRow(new Object[]{"09","Tran", "Van Cao", "0940459345", "tom@gmail.com",PassengerStatus.TICKETED});
+        passengerTable1.addRow(new Object[]{"10","Michael", "Jackson", "0983857491", "michael@gmail.com",PassengerStatus.TICKETED});
+        
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panel = new javax.swing.JLayeredPane();
+        card1 = new component.Card();
+        card2 = new component.Card();
+        card3 = new component.Card();
+        panelBorder1 = new swing.PanelBorder();
+        jLabel1 = new javax.swing.JLabel();
+        cmdAdding = new swing.AddingRowPanelAction();
+        jLabel2 = new javax.swing.JLabel();
+        spTable = new javax.swing.JScrollPane();
+        passengerTable1 = new swing.PassengerTable();
+
+        setPreferredSize(new java.awt.Dimension(859, 480));
+
+        panel.setLayout(new java.awt.GridLayout(1, 0, 10, 0));
+
+        card1.setColor1(new java.awt.Color(0, 102, 255));
+        card1.setColor2(new java.awt.Color(102, 153, 255));
+        panel.add(card1);
+
+        card2.setColor1(new java.awt.Color(186, 123, 247));
+        card2.setColor2(new java.awt.Color(167, 94, 236));
+        panel.add(card2);
+
+        card3.setColor1(new java.awt.Color(51, 153, 0));
+        card3.setColor2(new java.awt.Color(102, 204, 0));
+        panel.add(card3);
+
+        panelBorder1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(127, 127, 127));
+        jLabel1.setText("Passenger Table Design");
+
+        cmdAdding.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel2.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(127, 127, 127));
+        jLabel2.setText("Add Row");
+
+        passengerTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Passenger ID", "First name", "Last name", "Telephone", "Email", "Status", "Action"
+            }
+        ) {
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                if(columnIndex == 6){
+                    return true;
+                }
+                return rowIndex == editableRow && editable;
+            }
+        });
+        spTable.setViewportView(passengerTable1);
+
+        javax.swing.GroupLayout panelBorder1Layout = new javax.swing.GroupLayout(panelBorder1);
+        panelBorder1.setLayout(panelBorder1Layout);
+        panelBorder1Layout.setHorizontalGroup(
+            panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBorder1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(spTable)
+                    .addGroup(panelBorder1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 413, Short.MAX_VALUE)
+                        .addComponent(cmdAdding, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel2)))
+                .addContainerGap())
+        );
+        panelBorder1Layout.setVerticalGroup(
+            panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBorder1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(cmdAdding, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(spTable, javax.swing.GroupLayout.DEFAULT_SIZE, 162, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(panelBorder1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addGap(20, 20, 20))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(panelBorder1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private component.Card card1;
+    private component.Card card2;
+    private component.Card card3;
+    private swing.AddingRowPanelAction cmdAdding;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLayeredPane panel;
+    private swing.PanelBorder panelBorder1;
+    private swing.PassengerTable passengerTable1;
+    private javax.swing.JScrollPane spTable;
+    // End of variables declaration//GEN-END:variables
+}
