8f4c8bc8cf998b14e5442a8cf6b7db85e6791d98
binhbb2204 <binhbb2204@gmail.com>
2024-05-08 00:59:59 +0700

    making Public dashboard

---
+++ src/component/CustomerMenu.java
@@ -0,0 +1,134 @@
+
+package component;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JFrame;
+
+import event.EventMenuSelected;
+import model.Model_Menu;
+
+public class Menu extends javax.swing.JPanel {
+    private EventMenuSelected event;
+
+    public void addEventMenuSelected(EventMenuSelected event){
+        this.event = event;
+        listMenu1.addEventMenuSelected(event);
+    }
+    public Menu() {
+        initComponents();
+        setOpaque(false);
+        listMenu1.setOpaque(false);
+        init();
+        
+        
+    }
+    private void init(){
+        
+        listMenu1.addItem(new Model_Menu("", "Railway Data", Model_Menu.MenuType.TITLE));
+        listMenu1.addItem(new Model_Menu("1", "Train Table", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("2", "Schedule Table", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("5", "Station Table", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("5", "Couch Table", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("5", "Couch Type Table", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("2", "Track Table", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("2", "Journey Table", Model_Menu.MenuType.MENU));
+        
+        
+        listMenu1.addItem(new Model_Menu("", "Human-Resource Data", Model_Menu.MenuType.TITLE));
+        listMenu1.addItem(new Model_Menu("3", "Seat Table", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("3", "Ticket Table", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("4", "Passenger Table", Model_Menu.MenuType.MENU));
+ 
+        listMenu1.addItem(new Model_Menu("", "", Model_Menu.MenuType.EMPTY));
+        listMenu1.addItem(new Model_Menu("", "", Model_Menu.MenuType.EMPTY));
+        listMenu1.addItem(new Model_Menu("7", "Log out", Model_Menu.MenuType.MENU));
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panelMoving = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        listMenu1 = new swing.ListMenu<>();
+
+        panelMoving.setOpaque(false);
+
+        jLabel1.setFont(new java.awt.Font("SansSerif", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/train.png"))); // NOI18N
+        jLabel1.setText("Railway System");
+
+        javax.swing.GroupLayout panelMovingLayout = new javax.swing.GroupLayout(panelMoving);
+        panelMoving.setLayout(panelMovingLayout);
+        panelMovingLayout.setHorizontalGroup(
+            panelMovingLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(listMenu1, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        panelMovingLayout.setVerticalGroup(
+            panelMovingLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelMovingLayout.createSequentialGroup()
+                .addContainerGap(21, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(15, 15, 15)
+                .addComponent(listMenu1, javax.swing.GroupLayout.PREFERRED_SIZE, 522, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelMoving, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(panelMoving, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    @Override
+    protected void paintChildren(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        GradientPaint gp = new GradientPaint(0, 0, Color.decode("#3a7bd5"), 0, getHeight(), Color.decode("#3a6073"));
+        g2.setPaint(gp);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        g2.fillRect(getWidth() - 20, 0, getWidth(), getHeight());
+        super.paintChildren(g);
+    }
+
+    private int x;
+    private int y;
+
+    public void initMoving(JFrame frame){
+        panelMoving.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+               x = e.getX();
+               y = e.getY();
+            }
+        });
+        panelMoving.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                frame.setLocation(e.getXOnScreen() - x, e.getYOnScreen() - y);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private swing.ListMenu<String> listMenu1;
+    private javax.swing.JPanel panelMoving;
+    // End of variables declaration//GEN-END:variables
+}
