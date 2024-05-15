f96eb15b30bd966d26678969df0b6094bf113b78
binhbb2204 <binhbb2204@gmail.com>
2024-05-15 18:13:01 +0700

    bruh, whoever made this, needs to eat punch

--- src/swing/TicketTable.java
+++ src/swing/TicketTable.java
@@ -3,6 +3,9 @@ package swing;
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+
+import model.TrainType;
+
 import java.awt.*;
 
 public class TicketTable extends JTable{
@@ -14,14 +17,22 @@ public class TicketTable extends JTable{
         setRowHeight(40);
         getTableHeader().setReorderingAllowed(false);
 
+        // DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        // centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+
+        // // Apply the default renderer to the header and all cell types
+        // getTableHeader().setDefaultRenderer(centerRenderer);
+        // setDefaultRenderer(Object.class, centerRenderer);
+        // setDefaultRenderer(TrainType.class, centerRenderer);
+
         getTableHeader().setDefaultRenderer(new DefaultTableCellRenderer() {
             @Override
             public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
                     boolean hasFocus, int row, int column) {
                 TableHeader header = new TableHeader(value + "");
-                
-                header.setHorizontalAlignment(JLabel.CENTER);
-                
+                if (column == 10) {
+                    header.setHorizontalAlignment(JLabel.CENTER);
+                }
                 return header;
             }
         });
@@ -31,19 +42,24 @@ public class TicketTable extends JTable{
             public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
                     boolean hasFocus, int row, int column) {
                 try{
-                    
-                    Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus,
+                    if (column != 10) {
+                        Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus,
                                 row, column);
-                    component.setBackground(Color.WHITE);
-                    setBorder(noFocusBorder);
-                    if (isSelected) {
-                        component.setForeground(new Color(15, 89, 140));
-                    } 
-                    else {
-                        component.setForeground(new Color(102, 102, 102));
+                        component.setBackground(Color.WHITE);
+                        setBorder(noFocusBorder);
+                        if (isSelected) {
+                            component.setForeground(new Color(15, 89, 140));
+                        } else {
+                            component.setForeground(new Color(102, 102, 102));
+                        }
+                        setHorizontalAlignment(JLabel.CENTER);
+                        return component;
+                    } else {
+                        TrainType type = (TrainType) value;
+                        CellType cell = new CellType(type);
+                        setHorizontalAlignment(JLabel.CENTER);
+                        return cell;
                     }
-                    setHorizontalAlignment(JLabel.CENTER);
-                    return component;
                 }
                 catch (Exception e) {
                     // Handle rendering exception
