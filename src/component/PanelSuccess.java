180fccf7279603042c46c5cdf16cdb4f17fb1411
binhbb2204 <binhbb2204@gmail.com>
2024-05-18 14:44:04 +0700

    saving cuz máy đơ sos

---
+++ src/component/PanelSuccess.java
@@ -0,0 +1,117 @@
+
+package component;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+
+import model.Model_Error;
+
+public class PanelError extends javax.swing.JPanel {
+
+    public PanelError() {
+        initComponents();
+        setOpaque(false);
+        txt.setOpaque(false);
+        txt.setSelectionColor(new Color(173, 216, 230, 200));
+        txt1.setOpaque(false);
+        txt1.setSelectionColor(new Color(173, 216, 230, 200));
+    }
+
+    public void setData(Model_Error data){
+        lbTitle.setText(data.getTitle());
+    }
+
+    public PanelError(JLabel lbTitle){
+        this.lbTitle = lbTitle;
+    }
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fill(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 15, 15));
+        g2.dispose();
+        super.paintComponent(grphcs);
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        lbTitle = new javax.swing.JLabel();
+        txt = new javax.swing.JTextPane();
+        txt1 = new javax.swing.JTextPane();
+        jLabel2 = new javax.swing.JLabel();
+
+        setBackground(new java.awt.Color(53, 126, 199));
+        setBorder(javax.swing.BorderFactory.createEmptyBorder(25, 25, 25, 25));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 80)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText(":(");
+
+        lbTitle.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lbTitle.setForeground(new java.awt.Color(255, 255, 255));
+        lbTitle.setText("Title");
+
+        txt.setBackground(new java.awt.Color(53, 126, 199));
+        txt.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        txt.setForeground(new java.awt.Color(255, 255, 255));
+        txt.setText("Your PC ran into a problem and needs to restart as soon as we're finished collecting some error info.");
+
+        txt1.setBackground(new java.awt.Color(53, 126, 199));
+        txt1.setForeground(new java.awt.Color(255, 255, 255));
+        txt1.setText("For more information about this issue and possible fixes, please double-check the input data for any inaccuracies.");
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/qrcode.png"))); // NOI18N
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(68, 68, 68)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txt, javax.swing.GroupLayout.PREFERRED_SIZE, 446, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(txt1, javax.swing.GroupLayout.PREFERRED_SIZE, 357, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(lbTitle)))
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(235, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txt, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, Short.MAX_VALUE)
+                .addComponent(lbTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(txt1, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(36, 36, 36))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel lbTitle;
+    private javax.swing.JTextPane txt;
+    private javax.swing.JTextPane txt1;
+    // End of variables declaration//GEN-END:variables
+}
