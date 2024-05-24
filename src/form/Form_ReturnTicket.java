a98a72961fba34ea6a0e70a24bd4e6468a2241ff
binhbb2204 <binhbb2204@gmail.com>
2024-05-25 00:49:01 +0700

    saving progress

--- src/form/Form_ReturnTicket.java
+++ src/form/Form_ReturnTicket.java
@@ -1,11 +1,116 @@
 
 package form;
 
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
 
-public class Form_ReturnTicket extends javax.swing.JPanel {
+import connection.ConnectData;
+import glasspanepopup.GlassPanePopup;
+import model.Model_Error;
 
-    public Form_ReturnTicket() {
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import swing.ScrollBar;
+
+
+public class Form_BookingInformation extends javax.swing.JPanel {
+
+//SQL JDBC
+//-----------------------------------------------------------------------------------------------------
+    private void populateBookingInformationDatabase(String firstName, String lastName, String phoneNumber, String email){
+        String query = "SELECT " +
+               "    p.first_name, " +
+               "    p.last_name, " +
+               "    tr.trainName AS train_name, " +
+               "    ti.coachID, " +
+               "    s.seatNumber, " +
+               "    ds.stationName AS departure_station_name, " +
+               "    st.stationName AS arrival_station_name, " +
+               "    j.arrivalTime, " +
+               "    j.departureTime, " +
+               "    ti.departureDate, " +
+               "    ti.ticketPrice " +
+               "FROM " +
+               "    passenger p " +
+               "JOIN " +
+               "    ticket ti ON p.passengerID = ti.passengerID " +
+               "JOIN " +
+               "    train tr ON ti.trainID = tr.trainID " +
+               "JOIN " +
+               "    coach c ON ti.coachID = c.coachID " +
+               "JOIN " +
+               "    seat s ON ti.seatID = s.seatID " +
+               "JOIN " +
+               "    schedule sch ON ti.trainID = sch.trainID " +
+               "JOIN " +
+               "    station ds ON ti.departure_stationID = ds.stationID " +
+               "JOIN " +
+               "    station st ON ti.arrival_stationID = st.stationID " +
+               "JOIN " +
+               "    journey j ON sch.scheduleID = j.scheduleID AND ds.stationID = j.stationID " +
+               "WHERE " +
+               "    ti.departure_stationID = j.stationID " +
+               "    AND p.first_name = ? " +
+               "    AND p.last_name = ? " +
+               "    AND (p.phone_number = ? OR p.email = ?);";
+        try(Connection conn = new ConnectData().connect();
+            PreparedStatement pstmt = conn.prepareStatement(query)){
+            pstmt.setString(1, firstName);
+            pstmt.setString(2, lastName);
+            pstmt.setString(3, phoneNumber);
+            pstmt.setString(4, email);
+            
+            try( ResultSet rs = pstmt.executeQuery()){
+                DefaultTableModel model = (DefaultTableModel) table.getModel();
+                model.setRowCount(0);
+                if (!rs.isBeforeFirst()) { 
+                    GlassPanePopup.showPopup(Error);
+                    Error.setData(new Model_Error("Error: No tickets found for customer "+ firstName + " " + lastName));
+                    return;
+                }
+                while(rs.next()){
+                    spTable.setVisible(true);
+                    spTable.repaint();
+                    String trainName = rs.getString("train_name");
+                    String coachID = rs.getString("coachID");
+                    int seatNumber = rs.getInt("seatNumber");
+                    String departureStationName = rs.getString("departure_station_name");
+                    String arrivalStationName = rs.getString("arrival_station_name");
+                    String arrivalTime = rs.getString("arrivalTime");
+                    String departureTime = rs.getString("departureTime");
+                    String departureDate = rs.getString("departureDate");
+                    int ticketPrice = rs.getInt("ticketPrice");
+            
+                    model.addRow(new Object[]{firstName + " " + lastName, trainName, coachID, seatNumber, departureStationName, arrivalStationName, arrivalTime, departureDate, ticketPrice});
+                }
+            }
+            
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+//-----------------------------------------------------------------------------------------------------
+
+    public Form_BookingInformation() {
+        
         initComponents();
+        
+         
+        spTable.setVisible(false);
+        jLabel1.setVisible(false);
+        spTable.setVerticalScrollBar(new ScrollBar());
+        spTable.getVerticalScrollBar().setBackground(Color.WHITE);
+        JPanel p = new JPanel();
+        p.setBackground(Color.WHITE);
+        spTable.getViewport().setBackground(Color.WHITE);
+        spTable.setCorner(JScrollPane.UPPER_RIGHT_CORNER, p);
+
+        
     }
 
     /**
@@ -17,26 +122,152 @@ public class Form_ReturnTicket extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Error = new component.PanelError();
         panelRound1 = new swing.PanelRound();
+        lbFirstName = new javax.swing.JLabel();
+        txtFirstName = new swing.MyTextField();
+        lbLastName = new javax.swing.JLabel();
+        txtLastName = new swing.MyTextField();
+        lbPhoneNumber = new javax.swing.JLabel();
+        txtPhonenumber = new swing.MyTextField();
+        searchButton = new swing.Button();
+        panelBorder1 = new swing.PanelBorder();
+        spTable = new javax.swing.JScrollPane();
+        table = new swing.BookInformationTable();
+        jLabel1 = new javax.swing.JLabel();
+
+        lbFirstName.setFont(new java.awt.Font("SansSerif", 1, 16)); // NOI18N
+        lbFirstName.setForeground(new java.awt.Color(127, 127, 127));
+        lbFirstName.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        lbFirstName.setText("First Name:");
+
+        txtFirstName.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtFirstNameActionPerformed(evt);
+            }
+        });
+
+        lbLastName.setFont(new java.awt.Font("SansSerif", 1, 16)); // NOI18N
+        lbLastName.setForeground(new java.awt.Color(127, 127, 127));
+        lbLastName.setText("Last Name:");
+
+        lbPhoneNumber.setFont(new java.awt.Font("SansSerif", 1, 16)); // NOI18N
+        lbPhoneNumber.setForeground(new java.awt.Color(127, 127, 127));
+        lbPhoneNumber.setText("Phone Number/Email:");
+
+        searchButton.setBackground(new java.awt.Color(0, 102, 255));
+        searchButton.setForeground(new java.awt.Color(255, 255, 255));
+        searchButton.setText("Search");
+        searchButton.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        searchButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                searchButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout panelRound1Layout = new javax.swing.GroupLayout(panelRound1);
         panelRound1.setLayout(panelRound1Layout);
         panelRound1Layout.setHorizontalGroup(
             panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 848, Short.MAX_VALUE)
+            .addGroup(panelRound1Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(lbPhoneNumber, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lbFirstName, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelRound1Layout.createSequentialGroup()
+                        .addComponent(txtFirstName, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(lbLastName)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtLastName, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(35, 35, 35))
+                    .addGroup(panelRound1Layout.createSequentialGroup()
+                        .addComponent(txtPhonenumber, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelRound1Layout.createSequentialGroup()
+                .addContainerGap(356, Short.MAX_VALUE)
+                .addComponent(searchButton, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(356, 356, 356))
         );
         panelRound1Layout.setVerticalGroup(
             panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 156, Short.MAX_VALUE)
+            .addGroup(panelRound1Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbFirstName, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtFirstName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtLastName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lbLastName, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbPhoneNumber, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtPhonenumber, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(searchButton, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(14, Short.MAX_VALUE))
+        );
+
+        panelBorder1.setBackground(new java.awt.Color(255, 255, 255));
+
+        spTable.setBorder(null);
+
+        table.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Name", "Train Name", "Coach ID", "Seat Number", "From", "To", "Arrival Time", "Arrival Date", "Price"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        table.setSelectionBackground(new java.awt.Color(255, 255, 255));
+        spTable.setViewportView(table);
+
+        jLabel1.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(127, 127, 127));
+        jLabel1.setText("Booking Summary");
+
+        javax.swing.GroupLayout panelBorder1Layout = new javax.swing.GroupLayout(panelBorder1);
+        panelBorder1.setLayout(panelBorder1Layout);
+        panelBorder1Layout.setHorizontalGroup(
+            panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBorder1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelBorder1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(spTable))
+                .addContainerGap())
+        );
+        panelBorder1Layout.setVerticalGroup(
+            panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelBorder1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel1)
+                .addGap(20, 20, 20)
+                .addComponent(spTable, javax.swing.GroupLayout.DEFAULT_SIZE, 206, Short.MAX_VALUE)
+                .addGap(20, 20, 20))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(20, 20, 20)
-                .addComponent(panelRound1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(panelBorder1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(panelRound1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(20, 20, 20))
         );
         layout.setVerticalGroup(
@@ -44,12 +275,47 @@ public class Form_ReturnTicket extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addComponent(panelRound1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(316, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(panelBorder1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(17, 17, 17))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void txtFirstNameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtFirstNameActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtFirstNameActionPerformed
+
+    private void searchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchButtonActionPerformed
+        
+        String firstName = txtFirstName.getText();
+        String lastName = txtLastName.getText();
+        String phoneNumber = txtPhonenumber.getText();
+        
+        populateBookingInformationDatabase(firstName, lastName, phoneNumber, phoneNumber);
+        if (table.getModel().getRowCount() == 0) {
+            spTable.setVisible(false);
+            jLabel1.setVisible(false);
+        } else {
+            spTable.setVisible(true);
+            jLabel1.setVisible(true);
+        }
+        
+    }//GEN-LAST:event_searchButtonActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private component.PanelError Error;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel lbFirstName;
+    private javax.swing.JLabel lbLastName;
+    private javax.swing.JLabel lbPhoneNumber;
+    private swing.PanelBorder panelBorder1;
     private swing.PanelRound panelRound1;
+    private swing.Button searchButton;
+    private javax.swing.JScrollPane spTable;
+    private swing.BookInformationTable table;
+    private swing.MyTextField txtFirstName;
+    private swing.MyTextField txtLastName;
+    private swing.MyTextField txtPhonenumber;
     // End of variables declaration//GEN-END:variables
 }
